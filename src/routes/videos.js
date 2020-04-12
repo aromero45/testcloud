b690d9db0c4fe8cdda339e91d358db131bd69559
alejitro <alejitro@gmail.com>
2020-04-11 20:14:20 -0500

    Cambios a lista de videos

--- src/routes/videos.js
+++ src/routes/videos.js
@@ -12,80 +12,46 @@ const RUTA_GESTOR_ARCHIVOS = process.env.ruta_gestion_archivos;
 const pool = require('../database');
 //const {isLoggedIn} = require('../lib/auth');
 router.get ('/add', (req, res) => {
-     res.render('videos/addvideo');
+    const { url } = req.params;
+    console.log("URL add: ",url);
+    nameurl[0]=url;  
+    pool.query('SELECT * FROM contest WHERE url = ?', [url], function(err,result){
+      if(err){
+        throw err
+      }else{
+        const links=result[0];
+        console.log("Valor de links:", links);
+        res.render('videos/addvideo',{links});
+      }
+    }); 
+    
 });
 
 router.get('/:url', async (req, res) => {
 
   const { url } = req.params;
-  console.log(url);
+  console.log("URL",url);
+  nameurl[0]=url; 
+  const links = await pool.query('SELECT * FROM contest WHERE url = ?', [url]); 
+  const videos = await pool.query('SELECT *FROM videos WHERE contest_id = ?', [links[0].id]);
+  //res.render('videos/addvideo',{links});
+  res.render('videos/listvideos', {videos:videos, url:url});
+}); 
+
+router.get('/add/:url', async (req, res) => {
+
+  const { url } = req.params;
+  console.log("URL",url);
   nameurl[0]=url; 
   const links = await pool.query('SELECT * FROM contest WHERE url = ?', [url]); 
   const videos = await pool.query('SELECT *FROM videos WHERE contest_id = ?', [links[0].id]);
   console.log(links[0].id);
   console.log(videos);
-  res.render('videos/listvideos', {videos});
+  res.render('videos/addvideo',{links});
+  //res.render('videos/listvideos', {videos,links});
 }); 
 
-router.post('/add/url/:url', function (req, res, success){
-   
-        const { url } = req.params;
-        console.log("Url: ", url);
-        let contestid;
-
-        pool.query('SELECT * FROM contest WHERE url = ?',[url], function(err,result){
-            if (err){
-              throw err
-            }else{
-              contestid=result[0].id;
-              let originvideo
-              if (!req.files) {
-                originvideo = null;
-              }
-              else{
-                originvideo=req.files.originalvideo;
-              }
-              console.log("origin: ",req.files);
-              let nameVideo
-              originvideo===null?nameVideo='no-video':nameVideo=originvideo.name;   
-              console.log("Contest id: ",contestid);
-              console.log("nameVideo: ",nameVideo);
-              const newVideo = {
-                  name: req.body.name,
-                  last_name: req.body.lastname,
-                  email: req.body.email,
-                  message: req.body.message,
-                  original_video: nameVideo,
-                  contest_id: contestid
-              };
-              console.log("Video", newVideo.original_video);
-              pool.query('INSERT INTO videos set ?', [newVideo], function(err, res){
-                if(err){
-                  throw err
-                }else{
-                  if(fs.existsSync(RUTA_GESTOR_ARCHIVOS+contestid+'//inicial')){
-                      if(originvideo!==null){
-                          originvideo.mv(RUTA_GESTOR_ARCHIVOS+contestid+`//inicial//${originvideo.name}`,function(err, result){
-                              if(err){
-                                  return res.status(500).send(err);
-                              }
-                              success(result);
-                          });
-                      }
-                      else{
-                          success(result);
-                      }
-                  }
-                }
-              });
-            }
-        });
-        pool.query('SELECT * FROM contest WHERE url = ?', [url], function(err,result){
-          res.render('videos/listvideos', {result});
-        }); 
-});
-
-router.post('/add/id/:id', function (req, res, success){
+router.post('/add/id/:id', function (req, response, success){
    
   const { id } = req.params;
   let contestid;
@@ -116,7 +82,7 @@ router.post('/add/id/:id', function (req, res, success){
             contest_id: contestid
         };
         console.log("Video", newVideo.original_video);
-        pool.query('INSERT INTO videos set ?', [newVideo], function(err, res){
+        pool.query('INSERT INTO videos set ?', [newVideo], function(err){
           if(err){
             throw err
           }else{
@@ -124,26 +90,25 @@ router.post('/add/id/:id', function (req, res, success){
                 if(originvideo!==null){
                     originvideo.mv(RUTA_GESTOR_ARCHIVOS+contestid+`//inicial//${originvideo.name}`,function(err, result){
                         if(err){
-                            return res.status(500).send(err);
+                            throw err;
+                            //return res.status(500).send(err);
                         }
-                        success(result);
+                        pool.query('SELECT * FROM videos WHERE contest_id = ?', [id], function(err,result){
+                          console.log("Concurso: ", result);
+                          response.render('videos/listvideos', {result});
+                          //success(result);
+                        });
                     });
                 }
-                else{
+                /*else{
                     success(result);
-                }
+                }*/
             }
           }
         });
       }
   });
-  pool.query('SELECT * FROM contest WHERE id = ?', [id], function(err,result){
-    res.render('videos/listvideos', {result});
-  }); 
 });
-
-
-module.exports = router; 
 
 
 module.exports = router; 
